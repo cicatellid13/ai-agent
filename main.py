fdd5b5aef86f4df92cca939395d27f64de0244cb
cicatellid13 <dcicatelli100@gmail.com>
2025-06-21 22:35:19 -0400

    added calculator example app

--- main.py
+++ main.py
@@ -1,25 +1,49 @@
 import os
-from google import genai
+import sys
 from dotenv import load_dotenv
+from google import genai
+from google.genai import types
 
 
 
 def main():
     load_dotenv()
+    verbose = "--verbose" in sys.argv
+    args = list(filter(lambda arg: not arg.startswith("--"), sys.argv[1:]))
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage -> python main.py "your prompt here"')
+        print('Example -> python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+
+
     api_key = os.environ.get("GEMINI_API_KEY")
-
-
     client = genai.Client(api_key=api_key)
-    params = {
-        'model': 'gemini-2.0-flash-001',
-        'contents': 'Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
-    }
-    response = client.models.generate_content(
-        model=params["model"], contents=params["contents"]
-    )
-    print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    user_prompt = " ".join(args)
+
+    if verbose:
+         print(f"User prompt: {user_prompt}\n")
+    
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    query_model_with_output(client, messages, verbose)
+
+def query_model_with_output(client, messages, verbose):
+        response = client.models.generate_content(
+            model="gemini-2.0-flash-001", 
+            contents=messages,
+        )
+
+        if verbose:
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print("Response:")
+        print(response.text)
 
 
 if __name__ == "__main__":
