c5d4d5cfb61580859f68aba4e7465519e2e9312d
cicatellid13 <dcicatelli100@gmail.com>
2025-06-22 21:07:15 -0400

    added get_file_content function

---
+++ tests.py
@@ -0,0 +1,26 @@
+# from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
+
+
+
+def test():
+    result = get_file_content("calculator", "/bin/cat")
+    print("Result for 'lorem.txt' file:")
+    print(result)
+    print("")
+
+    result = get_file_content("calculator", "main.py")
+    print("Result for 'pkg' directory:")
+    print(result)
+
+    result = get_file_content("calculator", "pkg/calculator.py")
+    print("Result for '/bin' directory:")
+    print(result)
+
+    result = get_file_content("calculator", "/bin/cat")
+    print("Result for '../' directory:")
+    print(result)
+    
+
+if __name__ == "__main__":
+    test()
